754fc07ed06a5ef15020d25bbba40bb5956ff2ad
Jorge Ramirez <joramirez96@gmail.com>
2020-10-16 03:18:03 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,8 +14,7 @@ class Post < ActiveRecord::Base
     ]
 
   def must_be_clickbait
-    if clickbait_patterns.none?
-      {|p| p.match title}.errors.add(:title, "must_be_clickbait")
+    if clickbait_patterns.none?{|p| p.match title}errors.add(:title, "must_be_clickbait")
     end
   end
 
