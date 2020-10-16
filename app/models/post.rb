b0d021f941ee770d39b949bc78bd53d5f2cf4d03
Jorge Ramirez <joramirez96@gmail.com>
2020-10-16 02:11:12 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,7 @@ class Post < ActiveRecord::Base
 
   def must_be_clickbait
     if clickbait = ["Won't Believe", "Secret", "Top [number]", "Guess"]
-      clickbait.none?{|word| word.include?("clickbait")}
+      clickbait.none?{|word| word.include?("clickbait")}(:must_be_clickbait)
     end
   end
 end
